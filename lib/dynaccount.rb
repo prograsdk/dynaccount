6f315081b1939c9957e2b57855bf8dd74f3a84b1
Frederik Spang <frederik@frederikspang.dk>
2017-01-09 19:48:58 +0100

    Initialize values

--- lib/dynaccount.rb
+++ lib/dynaccount.rb
@@ -1,3 +1,4 @@
+require 'JSON'
 require 'faraday'
 require 'net/http/persistent'
 require 'logger'
--- lib/dynaccount/dynaccount_object.rb
+++ lib/dynaccount/dynaccount_object.rb
@@ -1,20 +1,24 @@
 module Dynaccount
   class DynaccountObject
-    def initialize(id)
-      @original_values = {}
-      @values = {}
-      @values[:id] = id if id
+    def initialize(attributes)
+      @values = attributes
+      @values.each do |k, v|
+        self.singleton_class.send(:attr_accessor, k.to_sym)
+        self.send("#{k}=", v)
+      end
     end
 
     def create(attributes = {})
     end
 
     def self.all
-      JSON.parse(find(nil)).to_hash
+      find(nil)
     end
 
     def self.find(id)
-      Dynaccount.request(url(id), {}, :post)
+      req = JSON.parse(Dynaccount.request(url(id), {}, :post).body)["result"].map { |res| new(res) }
+      return req[0] if req.size == 1
+      req
     end
 
     def self.url(id)
