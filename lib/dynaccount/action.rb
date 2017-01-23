de4f32ce7584d3d04280a3235bb8f2860e43a9df
Frederik Spang <frederik@frederikspang.dk>
2017-01-23 22:41:44 +0100

    Initial actions

--- lib/dynaccount/action.rb
+++ lib/dynaccount/action.rb
@@ -4,8 +4,12 @@ module Dynaccount
       'accounting_bank'
     end
 
-    def run(params = {})
-      params
+    def self.run(action, params = {})
+      JSON.parse(Dynaccount.request(url(action), params, :post).body)
+    end
+
+    def self.url(action)
+      "/v5/#{Dynaccount.api_id}/#{Dynaccount.api_key}/action/#{action}/json/"
     end
   end
 end
