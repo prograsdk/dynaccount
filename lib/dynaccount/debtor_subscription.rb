7696fdad5c684c9de9ca4332ffdd3a852ac257a1
Frederik Spang <frederik@frederikspang.dk>
2017-03-03 14:39:31 +0100

    Update put fields on debtor subscription

--- lib/dynaccount/debtor_subscription.rb
+++ lib/dynaccount/debtor_subscription.rb
@@ -1,7 +1,22 @@
 module Dynaccount
   class DebtorSubscription < DynaccountObject
     def self.ignore_put
-      [:id, :is_active, :module_id, :time_next_charge, :vat_amount, :total_amount, :total_weight, :profit_amount]
+      [
+        :id,
+        :is_active,
+        :module_id,
+        :module_group_id,
+        :module_name,
+        :module_vatno,
+        :module_ean,
+        :module_email,
+        :module_email_invoice,
+        :time_next_charge,
+        :vat_amount,
+        :total_amount,
+        :total_weight,
+        :profit_amount
+      ]
     end
 
     def self.api_path
