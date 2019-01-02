1ac6c9ad36d999d830cd3fa82fd50024894aa2e5
Frederik Spang <frederik@progras.dk>
2019-01-02 15:39:00 +0100

    Update API parameters for Debtor

--- lib/dynaccount/debtor.rb
+++ lib/dynaccount/debtor.rb
@@ -1,7 +1,16 @@
 module Dynaccount
   class Debtor < DynaccountObject
     def self.ignore_put
-      [:id, :contact_id, :balance, :vatno_validation, :time_vatno_validation, :due, :is_blocked]
+      [
+        :id,
+        :contact_id,
+        :balance,
+        :vatno_validation,
+        :time_vatno_validation,
+        :due,
+        :is_blocked,
+        :invoicing_module_name
+      ]
     end
 
     def self.api_path
